c3f89c06e0e5be4b1d9001dc45b7dd7f410f6acd
Gajanand sharma <gajanandsharmajsr@gmail.com>
2022-09-20 19:10:35 +0530

    screenshots

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -9,7 +9,7 @@ import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:date_time_picker/date_time_picker.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:localization/localization.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:duration/duration.dart';
 import '../widgets/header.dart' as head;
